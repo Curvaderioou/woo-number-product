b515dfcd02076a8ae4e6936ba0bb8fbda7982a17
Curvaderioou <felipedegenerportella@yahoo.com>
2021-10-30 16:28:15 -0300

    add depois do projeto final e do fim do estudo

--- functions.php
+++ functions.php
@@ -1,8 +1,78 @@
 <?php
 
-function handel_loop_shop_per_page(){
-  return 4;
+function handel_add_woocommerce_suport(){
+  add_theme_support('woocommerce');
 }
+add_action('after_setup_theme', 'handel_add_woocommerce_suport');
+
+
+/*
+function handel_css(){
+  wp_register_style('handel-style', get_template_directory_uri() . '/style.css')
+}
+
+echo get_template_directory_uri();
+*/
+function handel_css(){
+  wp_register_style('handel-style', get_template_directory_uri() . '/style.css', [], '1.0.0');
+  wp_enqueue_style('handel-style');
+}
+add_action('wp_enqueue_scripts', 'handel_css');
+
+function handel_custom_images(){
+  add_image_size('slide', 1000, 800, ['center', 'top']);
+  update_option('medium_crop', 1);
+}
+add_action('after_setup_theme', 'handel_custom_images');
+
+
+function handel_loop_shop_per_page(){
+  return 6;
+}
+
 add_filter('loop_shop_per_page', 'handel_loop_shop_per_page');
 
+include(get_template_directory() . '/inc/product-list.php');
+
+function remove_some_body_class($classes){
+ $woo_class = array_search('woocommerce', $classes);
+ $woopage_class = array_search('woocommerce-page', $classes);
+ $search = in_array('archive', $classes) || in_array('product-template-default', $classes);
+ if($woo_class && $woopage_class && $search){
+   unset($classes[$woo_class]);
+   unset($classes[$woopage_class]);
+ }
+ return $classes;
+}
+
+add_filter('body_class', 'remove_some_body_class');
+
+  include(get_template_directory() . '/inc/user-custom-menu.php');
+  
+  include(get_template_directory() . '/inc/checkout-customizado.php');
+
+/* O hook abaixo inclui algo no top do e-mail
+function handel_change_email_header(){
+  echo '<h2 style="text-align: center" >Mensagem Header</h2>';
+}
+add_action('woocommerce_email_header', 'handel_change_email_header');
+*/
+
+function handel_change_email_footer_text($text){
+  echo 'Handel
+  
+  <ul style="padding: 0px; margin: 0px; list-style: none;">
+    <li><a href="/">Facebook</a></li>
+    <li><a href="/">Instagram</a></li>
+    <li><a href="/">Twitter</a></li>
+  </ul>
+  ';
+}
+
+add_filter('woocommerce_email_footer_text', 'handel_change_email_footer_text');
+
+//abaixo conseguimos incluir tambem no email, se é para presente e se tem mensagem.
+
+
 ?>
+
